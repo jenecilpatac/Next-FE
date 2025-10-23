a129b620ad2681035351b02a9b3b530e2a4447ef
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-10-23 10:28:20 +0800

    new updates

--- src/app/chats/components/MessageBody.tsx
+++ src/app/chats/components/MessageBody.tsx
@@ -174,7 +174,7 @@ export default function MessageBody({
                     key={index}
                     className="relative flex gap-1 items-center hover:scale-125 transition-all duration-300 ease-in-out"
                     onClick={handleOpenUsersReactions(messageId)}
-                    title={users[0]
+                    title={users
                       ?.map((us: any) =>
                         us?.id === user?.id ? "You" : us?.name ?? "Anonymous"
                       )
